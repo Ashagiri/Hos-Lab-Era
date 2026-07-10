5f413dd0ea33cc83663f218a32510f103ff6c594
Ashagiri <ashagiri@example.com>
2026-07-10 10:45:42 +0545

    Refactor: Add login_required decorator to booking_view for enhanced user authentication

--- laboratory/views.py
+++ laboratory/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # This handles your primary landing dashboard page
 def home_view(request):
     # FIXED: Added the 'laboratory/' folder prefix so Django can find it perfectly!
     return render(request, 'laboratory/home.html')
+
+@login_required # <--- This line redirects unauthenticated users
+def booking_view(request):
+    return render(request, 'laboratory/booking.html')
     
