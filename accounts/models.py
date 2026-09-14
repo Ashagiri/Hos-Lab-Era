55a230bdc983a298675814ac075a0d5d8a014438
Ashagiri <giriasha0708@gmail.com>
2026-09-14 11:13:55 +0545

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    ROLE_CHOICES = (
+    ROLE_CHOICES = ( 
         ('admin', 'Admin'),
         ('patient', 'Patient'),
         ('technician', 'Technician'),
